65a1a60cef85cb0a4498508fe79e06ddfa115313
Jonathan_Lam <jonathan.lam.6830@gmail.com>
2026-07-26 18:50:31 -0500

    Update Password_checker.py

--- Password_checker.py
+++ Password_checker.py
@@ -2,10 +2,25 @@ print("Password Strength Checker")
 
 password = input("Enter a password: ")
 
+has_uppercase = False
+has_lowercase = False
+has_number = False
+#Checks if the password has at least one uppercase and/or lowercase letter
 for char in password:
     if char.isupper():
-        print("Your password contains an uppercase letter, which is good for security")
-# This code checks the length of the password
+        has_uppercase = True
+    if char.islower():
+        has_lowercase = True
+    if char.isdigit():
+        has_number = True
+if has_uppercase:
+    print("Your password has an uppercase letter, which is good for security.")
+if has_lowercase:
+    print("Your password has a lowercase letter, which is good for security.")
+if has_number:
+    print("Your password has a number, which is good for security.")
+
+#Checks the length of the password
 if len(password) < 8:
     print("Weak password: too short")
 else:
