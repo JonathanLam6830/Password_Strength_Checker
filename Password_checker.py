d3ed7af07ad5b7b08ed72868cc07d70aaadb67ec
Jonathan_Lam <jonathan.lam.6830@gmail.com>
2026-07-27 13:55:16 -0500

    Refactored project into multiple modules
    Moved resuable password functions into password_tools.py.

--- Password_checker.py
+++ Password_checker.py
@@ -1,41 +1,10 @@
-import string
-import random
-# Functions that check password characteristics
-def check_uppercase(password):
-    return any(char.isupper() for char in password)
-
-def check_lowercase(password):
-    return any(char.islower() for char in password)
-
-def check_number(password):
-    return any(char.isdigit() for char in password)
-
-def check_special(password):
-    return any(char in string.punctuation for char in password)
-
-def calculate_score(password):
-    score = 0
-    if check_uppercase(password):
-        score += 1
-    if check_lowercase(password):
-        score += 1
-    if check_number(password):
-        score += 1
-    if check_special(password):
-        score += 1
-    if len(password) >= 8:
-        score += 1
-    return score
-
-def generate_password(length=12):
-    characters= string.ascii_letters + string.digits + string.punctuation
-
-    password = ""
-
-    for i in range(length):
-        password += random.choice(characters)
-
-    return password
+from password_tools import (check_uppercase, 
+                            check_lowercase,
+                            check_number,
+                            check_special,
+                            calculate_score,
+                            generate_password,
+)
 
 is_common_password = False
 # List of frequently used passwords that should be avoided
